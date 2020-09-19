58d3385b14a97379f454dd334bb7ccbafffd0a5f
ZyabrovaOlga <ingasholga@gmail.com>
2020-09-19 22:42:05 +0700

    promises

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-let name = 'defunktpppp';
+let name = 'ZyabrovaOlga';
 let newUser;
 let href = document.location.href;
 const userAva = document.getElementById('avatar');
 const userName = document.getElementById('name');
 const userBio = document.getElementById('bio');
 const userLink = document.getElementById('link');
+const date = document.getElementById('date');
+
+let preloader = document.getElementById('preloader');
+
+setTimeout(function() {
+	preloader.classList.add('hidden');
+}, 2000);
 
 const requestUrl = function () {
   let user = href.split('=');
   if (user[1]) {
     username = user[1];
   } else {
-    username = name;
+    username = name
   };
   return 'https://api.github.com/users/' + username;
 }
 
 let apiLink = requestUrl();
+
+let currentDate = new Date();
+
+const getDate = new Promise((resolve, reject) => {
+  setTimeout(() => currentDate ? resolve(date.innerHTML = currentDate.toDateString()) : reject('Не удалось получить дату'), 2000);
+})
 
 const renderCard = function (obj) {
   userAva.src = obj.avatar_url;
@@ -30,7 +43,9 @@ const renderCard = function (obj) {
 }
 
 fetch(apiLink)
+Promise.all([getDate])
+    .then(() => fetch(apiLink))
     .then(res => res.json())
     .then(obj => newUser = Object.assign({}, obj))
     .then(newUser => renderCard(newUser))
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
